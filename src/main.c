7ea1afd75298d13360f8852d50cc60f40710b730
Max Sills <sillsm@gmail.com>
2021-05-23 12:46:16 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "misc.h"
 #include "main.h"
+#include <stdint.h>
 
 // Annoying secret word that needs to go at the
 // end of the binary so the default bootloader doesn't
@@ -17,71 +18,65 @@ void delay(int millis) {
     }
 }
 
-typedef union{
-  struct LCDPinLayout{
-  unsigned int rs : 1;
-  unsigned int rw : 1;
-  unsigned int d7 : 1;
-  unsigned int d6 : 1;
-  unsigned int d5 : 1;
-  unsigned int d4 : 1;
-  unsigned int d3 : 1;
-  unsigned int d2 : 1;
-  unsigned int d1 : 1;
-  unsigned int d0 : 1;
-  } field;
-  unsigned short whole;
-} LCD;
+#define BITFIELD10(x0,x1,x2,x3,x4,x5,x6,x7,x8,x9)\
+((bitfield){10,0b##x0##x1##x2##x3##x4##x5##x6##x7##x8##x9})
 
-/*
- Convert an LCD struct to the correct
- wiring in the MPK 249.
-*/
+typedef struct BetterBitfield{
+    int size;
+    uint32_t field;
+} bitfield;
+
+// Bit bang an enable pulse to the LCD.
 void lcdpulse(){
   GPIOB->BSRR  = 0x1;
-  delay(40);
+  delay(2);
   GPIOB->BSRR  = 0x10000;
 }
 
-void LCDToWires(LCD l){
-  // B1 and B2 do the control stuff.
-  const unsigned short controlbits = l.field.rs << 2 | l.field.rw << 1;
+/*
+ Convert a bitfield to the correct
+ wiring in the MPK 249.
+*/
+void LCDToWires2(bitfield b){
+  // TODO -- static assert the bitfield has size 10
 
-  GPIOB->BSRR = controlbits | (~controlbits << 16);
+  // B1 and B2 do the control stuff.
+  uint32_t controlbits = (b.field >> 8) << 1;
+
+  // Twiddle only the first 8 bits. So send ~ of the 8
+  // to the upper register of BSRR.
+  uint32_t controlmask = 0b00000000000000000000000000000011;
+  GPIOB->BSRR = controlbits | (~controlbits & controlmask) << 16;
 
   // Port C0-7 does the data stuff.
-  const unsigned short databits = l.field.d7 << 7 | l.field.d6 << 6 |
-        l.field.d5 << 5 | l.field.d4 << 4 | l.field.d3 << 3 |
-        l.field.d2 << 2 | l.field.d1 << 1 | l.field.d0;
+  uint32_t databits = b.field&0b0011111111;
 
-  GPIOC->BSRR = databits | (~databits << 16);
+  uint32_t datamask = 0b00000000000000000000000011111111;
+//  x | ((~x & datamask) << 16)
+  GPIOC->BSRR = databits | (~databits & datamask) << 16;
   // Send the clock pulse to make the LCD process the instruction.
   lcdpulse();
 }
 
 // Initialize LCD display
 void initDisplay(void){
-  LCD l1 = {{0, 0, 0, 0, 1, 1, 1, 1, 1, 1}};
+  bitfield init       = BITFIELD10(0,0,0,0,1,1,1,1,1,1);
+  bitfield displayOff = BITFIELD10(0,0,0,0,0,0,1,0,0,0);
+  bitfield clear      = BITFIELD10(0,0,0,0,0,0,0,0,0,1);
+  bitfield entryMode  = BITFIELD10(0,0,0,0,0,0,0,1,1,0);
+  bitfield blink      = BITFIELD10(0,0,0,0,0,0,1,1,1,1);
+
   delay(50);
-  LCDToWires(l1);
+  LCDToWires2(init);
   delay(10);
-  LCDToWires(l1);
   delay(10);
-  LCDToWires(l1);
+  LCDToWires2(init);
   delay(10);
-  LCDToWires(l1);
-
-  LCD displayOFF = {{0, 0, 0, 0 ,0, 0, 1, 0, 0, 0}};
-  LCDToWires(displayOFF);
-  LCD clear = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 1}};
-  LCDToWires(clear);
-  LCD entryMode = {{0, 0, 0, 0, 0, 0, 0, 1, 1, 1}};
-  LCDToWires(entryMode);
-
-
-  LCD blink = {{0, 0, 0, 0, 0, 0, 1, 1, 1, 1}};
-  LCDToWires(blink);
-
+  LCDToWires2(init);
+  LCDToWires2(displayOff);
+  LCDToWires2(clear);
+  LCDToWires2(entryMode);
+  LCDToWires2(blink);
 }
 
 void copyPatterns(){
@@ -90,15 +85,35 @@ void copyPatterns(){
   GPIOC->CRL = 0x33333333;
 }
 
+void contrast(){
+  // Everything required to set TIM3 to pulse
+  // via PORTB5 to the LCD's potentiometer.
+  TIM3 -> CR1 = 0x81;
+  TIM3 -> SR  = 0x1f;
+  TIM3 -> CCMR1 = 0x00006800;
+  TIM3 -> CCER = 0x10;
+  TIM3 -> ARR  = 0x000001df;
+  TIM3 -> CCR2 = 0x00000155; // This is the contrast value.
+  TIM3 -> DMAR = 0x00000081;
+}
+
+void writeChar (char c){
+  bitfield b = BITFIELD10(1, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+  b.field = b.field | c;
+  LCDToWires2(b);
+}
+
+void writeString(char* s){
+  int i;
+  for (i = 0; s[i]!='\0';i++) {
+    writeChar(s[i]);
+  }
+}
+
 int main(void) {
     // Push it forward
     NVIC_SetVectorTable(NVIC_VectTab_FLASH, 0x6000);
 
-    // Try this
-    //GPIOC->BSRR = GPIO_BSRR_BR13;
-    // Is this the blink?
-    //#GPIOE->BSRR = 0x4000;
-    // GPIO structure for port initialization
     GPIO_InitTypeDef GPIO_InitStructure;
 
     // enable clock on APB2
@@ -106,8 +121,12 @@ int main(void) {
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,  ENABLE);
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,  ENABLE);
 
-    // configure port A1 for driving an LED
-    // GPIO_Pin_All
+   // The three things you need to do to hook up Tim3_ch2
+   // PortB5 output. Where the LCD potentiometer is connected.
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+    GPIO_PinRemapConfig(GPIO_PartialRemap_TIM3, ENABLE);
+
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;    // output push-pull mode
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;   // highest speed
@@ -118,18 +137,11 @@ int main(void) {
 
     copyPatterns();
 
-
     // LCD logic
     initDisplay();
+    contrast();
+    writeString("  OPEN KLAVE  ");
+
     while(1){};
 
-
-    // main loop
-    while(1) {
-        GPIO_SetBits(GPIOE, 0x4000);    // turn the LED on
-        delay(300);
-
-        GPIO_ResetBits(GPIOE, 0x4000);  // turn the LED off
-        delay(300);
-    }
 }
