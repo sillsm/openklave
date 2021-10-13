f5da8a17a697841ac4452738d58f71cc2895b644
Max Sills <sillsm@gmail.com>
2021-10-13 14:22:12 -0700

    Separate kernel from user space applications making API calls

--- src/kernel.h
+++ src/kernel.h
@@ -1,9 +1,18 @@
-#ifndef __MAIN_H
-#define __MAIN_H
+#ifndef __KERNEL_H
+#define __KERNEL_H
 
 #include <stm32f10x.h>
 #include <stm32f10x_gpio.h>
 #include <stm32f10x_rcc.h>
+
+int start(void);
+
+
+
+
+
+
+
 
 #define DELAY 1500  // in millis
 
